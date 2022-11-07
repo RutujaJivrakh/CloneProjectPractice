7e0702b62024f8ed98006184424315ce90b01fa1
sabakokane <sabaikokane@gmail.com>
2022-11-07 15:54:13 +0530

    welcome controller added

---
+++ FirstGitProject/src/main/java/com/Rutuja/Controller/Saba/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.Rutuja.Controller.Saba;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("/loginlink")
+	public String msg(Model model) {
+	String m="Welcome to new project";
+	model.addAttribute("LOGIN",m);
+	return "login";
+		
+		
+		
+	}
+
+}
