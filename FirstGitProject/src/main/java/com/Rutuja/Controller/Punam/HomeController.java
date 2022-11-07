96818bbe892a4f6ac392006a48c863633895f9d0
punamdadas123 <punamkisandadas@gmail.com>
2022-11-07 15:46:23 +0530

    Home Controller Added

---
+++ FirstGitProject/src/main/java/com/Rutuja/Controller/Punam/HomeController.java
@@ -0,0 +1,15 @@
+package com.Rutuja.Controller.Punam;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+	public String home() {
+		
+		System.out.println("Welcome to Bikkad IT 04 Team");
+		
+		return null;
+		
+	}
+}
